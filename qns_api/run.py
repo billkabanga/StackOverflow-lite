6670a849ef02a4d2f84109d011d7caba883f6832
Kabanga Bill <tkbillkabanga@gmail.com>
2018-08-19 10:11:28 +0300

    Modifies the run.py file[#159850480]

--- qns_api/run.py
+++ qns_api/run.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from instance.config import DevelopmentConfig
-from api import create_app, app
+from api import create_app
+from api import app
 
 app = create_app(DevelopmentConfig)
 
-
+#this conditional statememnt runs the application
 if __name__ == '__main__':
     app.run()
