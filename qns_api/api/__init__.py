4f52b9c1c467b92375d4052c1619e002d98ed767
Kabanga Bill <tkbillkabanga@gmail.com>
2018-08-24 10:31:16 +0300

    Modifies create app

--- qns_api/api/__init__.py
+++ qns_api/api/__init__.py
@@ -3,8 +3,8 @@ from qns_api.instance.config import DevelopmentConfig
 from qns_api.api.app import my_blue_print
 
 #Function below creates the app,gives it a name,sets the configuration mode and registers my bluepritn that has the routes.
-def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
+def create_app(DevelopmentConfig):
+    app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
     app.register_blueprint(my_blue_print)
 
